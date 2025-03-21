adf576a817bc5d191ebc064d59a4518efdd01220
Masha Ponomarenko <maria@Maria.local>
2025-03-21 20:36:59 +0200

    add task solution

---
+++ src/components/Clock.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+interface Props {
+  clockName: string;
+}
+
+export class Clock extends React.Component<Props> {
+  state = {
+    today: new Date(),
+  };
+
+  handleTimeUpdate = window.setInterval(() => {
+    this.setState({ ...this.state, today: new Date() });
+    // eslint-disable-next-line no-console
+    console.log(this.state.today.toUTCString().slice(-12, -4));
+  }, 1000);
+
+  componentDidUpdate(prevProps: Readonly<Props>): void {
+    if (prevProps.clockName !== this.props.clockName) {
+      // eslint-disable-next-line no-console
+      console.warn(
+        `Renamed from ${prevProps.clockName} to ${this.props.clockName}`,
+      );
+    }
+  }
+
+  componentWillUnmount(): void {
+    window.clearInterval(this.handleTimeUpdate);
+  }
+
+  render() {
+    const { clockName } = this.props;
+    const { today } = this.state;
+
+    return (
+      <div className="Clock">
+        <strong className="Clock__name">{clockName}</strong>
+
+        {' time is '}
+
+        <span className="Clock__time">
+          {today.toUTCString().slice(-12, -4)}
+        </span>
+      </div>
+    );
+  }
+}
